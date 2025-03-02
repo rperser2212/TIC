6c100ce1862cd86821cb304b4727ef55b106c0f6
rperser2212 <rperser2212@g.educaand.es>
2025-03-02 16:55:20 +0100

    Create comprobarsiespar.js

---
+++ comprobarsiespar.js
@@ -0,0 +1,15 @@
+function comprobarsiespar (numero)
+  {
+    var esPar;
+      
+        if (numero % 2 ===0)
+        {
+          esPar = true;
+          return esPar;
+        }
+ 
+       else   {
+              esPar = false;
+              return esPar;
+              }
+  }
