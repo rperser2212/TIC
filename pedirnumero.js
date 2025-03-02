9a8e03e8ba49884145ebaf39f105043e4887170a
rperser2212 <rperser2212@g.educaand.es>
2025-03-02 16:57:01 +0100

    Create pedirnumero.js

---
+++ pedirnumero.js
@@ -0,0 +1,7 @@
+var numero = prompt ("Escribe un numero del 1 al 100"); 
+            var i = 1;
+            var suma = 0;
+            
+          while (numero<1 || numero >100) { 
+                    numero = prompt ("este numero NO nos vale, introduce uno valido");
+                      }
